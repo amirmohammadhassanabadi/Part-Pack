c40265a8c0272425ce32bdd70ad1004b05e94e16
amir <hasanabadi.am1380@gmail.com>
2026-05-11 12:19:12 +0330

    Feat: vehicles module models added

---
+++ src/modules/vehicles/model/carModel.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const carModelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      index: true
+    },
+
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
+      index: true
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+      index: true
+    }
+  },
+  { timestamps: true }
+);
+
+carModelSchema.index({ name: 1, brand: 1 }, { unique: true });
+
+module.exports = mongoose.model("CarModel", carModelSchema);
